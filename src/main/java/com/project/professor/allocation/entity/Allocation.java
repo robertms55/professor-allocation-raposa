2f1a2e5a05a2a5007f207e0a243ca6bbf950637f
Roberto <roberto.moreira99@gmail.com>
2025-02-22 16:33:09 -0300

    feat: Add relationships between classes

---
+++ src/main/java/com/project/professor/allocation/entity/Allocation.java
@@ -0,0 +1,11 @@
+package com.project.professor.allocation.entity;
+
+import java.sql.Time;
+import java.time.DayOfWeek;
+
+public class Allocation {
+private Long id; 
+private DayOfWeek day;
+private Time start;
+private Time end;
+}
---
+++ src/main/java/com/project/professor/allocation/entity/Course.java
@@ -0,0 +1,7 @@
+package com.project.professor.allocation.entity;
+
+public class Course {
+private Long id; 
+private String name; 
+
+}
