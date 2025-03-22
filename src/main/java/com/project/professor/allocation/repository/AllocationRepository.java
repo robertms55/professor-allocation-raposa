f40cb13afc2857b8aaa7a741df59e8872b9e16fb
Roberto <roberto.moreira99@gmail.com>
2025-03-22 16:58:12 -0300

    Feat: including interfaces

--- src/main/java/com/project/professor/allocation/repository/AllocationRepository.java
+++ src/main/java/com/project/professor/allocation/repository/AllocationRepository.java
@@ -1,5 +1,17 @@
 package com.project.professor.allocation.repository;
 
-public interface AllocationRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.professor.allocation.entity.Allocation;
+import com.project.professor.allocation.entity.Course;
+import com.project.professor.allocation.entity.Professor;
+
+public interface AllocationRepository extends JpaRepository<Allocation, Long> {
+
+	List<Allocation> findByProfessor(Professor professor);
+	List <Allocation> findByCourse (Course course);
+	
+	
 }
