df290d259c541fd75000b21001df0ab1f133e884
Roberto <roberto.moreira99@gmail.com>
2025-03-22 16:58:12 -0300

    Feat: including interfaces

--- src/main/java/com/project/professor/allocation/controller/AllocationController.java
+++ src/main/java/com/project/professor/allocation/controller/AllocationController.java
@@ -1,9 +1,6 @@
 package com.project.professor.allocation.controller;
 
-import java.sql.Time;
 import java.time.DayOfWeek;
-import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -19,31 +16,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.professor.allocation.entity.Allocation;
+import com.project.professor.allocation.repository.AllocationRepository;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
-
 @RestController
 @RequestMapping(path = "/allocations")
 public class AllocationController {
+	private final AllocationRepository allocationRepository;
+
+	public AllocationController(AllocationRepository allocationRepository) {
+		super();
+		this.allocationRepository = allocationRepository;
+	}
 
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Allocation>> findAll() {
-		List<Allocation> allocations = new ArrayList<>();
-
-		Allocation alloc = new Allocation();
-		alloc.setDay(DayOfWeek.SATURDAY);
-		alloc.setStart(Time.valueOf(LocalTime.of(8, 00)));
-		alloc.setEnd(Time.valueOf(LocalTime.of(17, 00)));
-
-		allocations.add(alloc);
-
+		List<Allocation> allocations = allocationRepository.findAll();
 		return new ResponseEntity<>(allocations, HttpStatus.OK);
 	}
-	
+
 	@GetMapping(path = "/{allocation_id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Allocation> findById(@PathVariable(name = "allocation_id") Long id) {
 		if (id % 2 == 0) {
@@ -53,30 +48,29 @@ public class AllocationController {
 			return new ResponseEntity<>(allocation, HttpStatus.OK);
 		}
 	}
-	
+
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Allocation> save(@RequestBody Allocation allocation) {
 		allocation.setId(550l);
-	    return new ResponseEntity<>(allocation, HttpStatus.CREATED);
+		return new ResponseEntity<>(allocation, HttpStatus.CREATED);
 	}
-	
+
 	@Operation(summary = "Update a professor")
-    @ApiResponses({
-    	@ApiResponse(responseCode = "200", description = "OK"),
-    	@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-    	@ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
-    })
+	@ApiResponses({ @ApiResponse(responseCode = "200", description = "OK"),
+			@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+			@ApiResponse(responseCode = "404", description = "Not Found", content = @Content) })
 	@PutMapping(path = "/{allocation_id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Allocation> update(@PathVariable(name = "allocation_id") Long id, @RequestBody Allocation allocation) {
+	public ResponseEntity<Allocation> update(@PathVariable(name = "allocation_id") Long id,
+			@RequestBody Allocation allocation) {
 		allocation.setDay(DayOfWeek.TUESDAY);
-        return new ResponseEntity<>(allocation, HttpStatus.OK);
+		return new ResponseEntity<>(allocation, HttpStatus.OK);
 	}
-	
+
 	@DeleteMapping(path = "/{allocation_id}")
 	public ResponseEntity<Void> delete(@PathVariable(name = "allocation_id") Long id) {
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
-	
+
 	@DeleteMapping
 	public ResponseEntity<Void> deleteAll() {
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
--- src/main/java/com/project/professor/allocation/repository/ProfessorRepository.java
+++ src/main/java/com/project/professor/allocation/repository/ProfessorRepository.java
@@ -1,5 +1,17 @@
 package com.project.professor.allocation.repository;
 
-public interface ProfessorRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.professor.allocation.entity.Department;
+import com.project.professor.allocation.entity.Professor;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+
+	List<Professor> findByNameContainingIgnoreCase(String name);
+
+	List<Professor> findByDepartment(Department department);
 }
