e1ba6f549aa0146fb0a0b2d0b5541c07bdd1896e
O <sla@gmail.com>
2025-03-22 15:48:29 -0300

    Add

--- src/main/java/com/project/professor/allocation/entity/Course.java
+++ src/main/java/com/project/professor/allocation/entity/Course.java
@@ -7,9 +7,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Course {
 
