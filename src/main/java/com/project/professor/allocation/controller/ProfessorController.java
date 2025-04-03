06eea4672069a81a9fcc8acbf4f90dc194e6cd68
robertms15 <roberto.moreira99@gmail.com>
2025-04-03 00:43:35 -0300

    feat: add controller class for Department and Professor

--- src/main/java/com/project/professor/allocation/controller/ProfessorController.java
+++ src/main/java/com/project/professor/allocation/controller/ProfessorController.java
@@ -1,5 +1,104 @@
 package com.project.professor.allocation.controller;
 
-public class ProfessorController {
+import com.project.professor.allocation.entity.Professor;
+import com.project.professor.allocation.repository.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+
+@RestController
+@RequestMapping(path = "/professors")
+public class ProfessorController {
+private final ProfessorRepository repository;
+public ProfessorController (ProfessorRepository repository){
+    super();
+    this.repository = repository;
 }
+
+    @Operation(summary = "Find a professor ")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
+    })
+
+    @GetMapping(path = "/{professor_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Professor> findById(@PathVariable(name = "professor_id") Long id) {
+        Professor professor = repository.findById(id).orElse(null);
+        if (professor == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(professor, HttpStatus.OK);
+        }
+    }
+
+    @Operation(summary = "Save a professor")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Created"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
+    })
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Professor> save(@RequestBody Professor professor) {
+        try {
+            professor = repository.save(professor);
+            return new ResponseEntity<>(professor, HttpStatus.CREATED);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
+    }
+
+
+    @Operation(summary = "Update a professor")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
+    })
+    @PutMapping(path = "/{professor_id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Professor> update(@PathVariable(name = "professor_id") Long id,
+                                             @RequestBody Professor professor) {
+        professor.setId(id);
+        try {
+            professor = repository.save(professor);
+            if (professor == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            } else {
+                return new ResponseEntity<>(professor, HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
+
+    }
+        @Operation(summary = "Delete a professor")
+        @ApiResponses({
+                @ApiResponse(responseCode = "204", description = "No Content"),
+                @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
+        })
+        @DeleteMapping(path = "/{professor_id}")
+        public ResponseEntity<Void> deleteById(@PathVariable(name = "professor_id") Long id) {
+            repository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        @Operation(summary = "Delete all professors")
+        @ApiResponses({
+                @ApiResponse(responseCode = "204", description = "No Content")
+        })
+        @DeleteMapping
+        public ResponseEntity<Void> deleteAll() {
+            repository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+
+    }
+
+
--- src/main/java/com/project/professor/allocation/entity/Department.java
+++ src/main/java/com/project/professor/allocation/entity/Department.java
@@ -9,9 +9,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Department {
 
