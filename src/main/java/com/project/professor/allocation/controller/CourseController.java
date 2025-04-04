9a2df643fc915ce7bcee0e93da64dc84dc2a20b4
Samuel Lima <samuellima0808xd@gmail.com>
2025-04-04 20:43:45 -0300

    feat: updating controller class and add controller for Course

--- src/main/java/com/project/professor/allocation/controller/CourseController.java
+++ src/main/java/com/project/professor/allocation/controller/CourseController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +20,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Courses")
 @RestController
 @RequestMapping(path = "/Courses")
 public class CourseController {
-	private final CourseRepository courseRepository;
 	
+	private final CourseRepository courseRepository;
 	
 	public CourseController(CourseRepository courseRepository) {
 		super();
@@ -39,6 +43,21 @@ public class CourseController {
 			return new ResponseEntity<>(course, HttpStatus.OK);
 		}
 	}
+	
+    @Operation(summary = "Save a course")
+    @ApiResponses({
+    	@ApiResponse(responseCode = "201", description = "Created"),
+    	@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)
+    })
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Course> save(@RequestBody Course course) {
+        try {
+            course = courseRepository.save(course);
+            return new ResponseEntity<>(course, HttpStatus.CREATED);
+        } catch (Exception e) {
+        	throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
+    }
 	
 	@Operation(summary = "Update a Course")
 	@ApiResponses({ @ApiResponse(responseCode = "200", description = "OK"),
@@ -66,6 +85,16 @@ public class CourseController {
 	@DeleteMapping(path = "/{course_id}")
 	public ResponseEntity<Void> deleteById(@PathVariable(name = "course_id") Long id) {
 		courseRepository.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+	
+	@Operation(summary = "Delete all Courses")
+	@ApiResponses({
+		@ApiResponse(responseCode = "204", description = "No Content")
+	})
+	@DeleteMapping
+	public ResponseEntity<Void> deleteAll() {
+		courseRepository.deleteAll();
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
