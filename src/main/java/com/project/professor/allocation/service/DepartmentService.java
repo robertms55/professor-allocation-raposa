4ef1b5ef6b0d7b943760da0157d8b5a44d71c44d
Roberto <roberto.moreira99@gmail.com>
2025-04-05 15:22:05 -0300

    feat: add professor and department services

--- src/main/java/com/project/professor/allocation/service/DepartmentService.java
+++ src/main/java/com/project/professor/allocation/service/DepartmentService.java
@@ -1,8 +1,11 @@
 package com.project.professor.allocation.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.project.professor.allocation.entity.Department;
+import com.project.professor.allocation.entity.Professor;
 import com.project.professor.allocation.repository.DepartmentRepository;
 
 @Service
@@ -17,6 +20,41 @@ public class DepartmentService {
 
 	public Department findById(Long id) {
 		return repository.findById(id).orElse(null);
+
+	}
+
+	public List<Department> findAll(String name) {
+		if (name == null) {
+			return repository.findAll();
+		} else {
+			return repository.findByNameContainingIgnoreCase(name);
+		}
+	}
+
+	public Department save(Department department) {
+		department.setId(null);
+		return repository.save(department);
+	}
+
+	public Department update(Department department) {
+		Long id = department.getId();
+		if (id != null && repository.existsById(id)) {
+			return repository.save(department);
+		} else {
+			return null;
+		}
+
+	}
+
+	public void deleteById(Long id) {
+
+		if (id != null && repository.existsById(id)) {
+			repository.deleteById(id);
+		}
+	}
+
+	public void deleteAll() {
+		repository.deleteAllInBatch();
 
 	}
 
