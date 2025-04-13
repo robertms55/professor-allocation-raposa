30d3b86cfffa7a8d6fbdf9b8fe9940a930c01f12
robertms15 <roberto.moreira99@gmail.com>
2025-04-13 07:41:58 -0300

    feat incluind course service and controller

--- src/main/java/com/project/professor/allocation/controller/DepartmentController.java
+++ src/main/java/com/project/professor/allocation/controller/DepartmentController.java
@@ -1,5 +1,110 @@
 package com.project.professor.allocation.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import com.project.professor.allocation.entity.Department;
+import com.project.professor.allocation.service.DepartmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+@Tag(name = "Departments")
+@RestController
+@RequestMapping(path = "/departments")
 public class DepartmentController {
 
+	private final DepartmentService service;
+
+	public DepartmentController(DepartmentService service) {
+		super();
+		this.service = service;
+	}
+
+	@Operation(summary = "Find a department")
+	@ApiResponses({ @ApiResponse(responseCode = "200", description = "OK"),
+			@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+			@ApiResponse(responseCode = "404", description = "Not Found", content = @Content) })
+	@GetMapping(path = "/{department_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Department> findById(@PathVariable(name = "department_id") Long id) {
+		Department department = service.findById(id);
+		if (department == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(department, HttpStatus.OK);
+		}
+	}
+
+	@Operation(summary = "Find all professors")
+	@ApiResponses({ @ApiResponse(responseCode = "200", description = "OK") })
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Department>> findAll(@RequestParam(name = "name", required = false) String name) {
+		List<Department> professors = service.findAll(name);
+		return new ResponseEntity<>(professors, HttpStatus.OK);
+	}
+
+	@Operation(summary = "Save a department")
+	@ApiResponses({ @ApiResponse(responseCode = "201", description = "Created"),
+			@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content) })
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Department> save(@RequestBody Department department) {
+		try {
+			department = service.save(department);
+			return new ResponseEntity<>(department, HttpStatus.CREATED);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+		}
+	}
+
+	@Operation(summary = "Update a department")
+	@ApiResponses({ @ApiResponse(responseCode = "200", description = "OK"),
+			@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+			@ApiResponse(responseCode = "404", description = "Not Found", content = @Content) })
+	@PutMapping(path = "/{department_id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Department> update(@PathVariable(name = "department_id") Long id,
+			@RequestBody Department department) {
+		department.setId(id);
+		try {
+			department = service.update(department);
+			if (department == null) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			} else {
+				return new ResponseEntity<>(department, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+		}
+	}
+
+	@Operation(summary = "Delete a department")
+	@ApiResponses({ @ApiResponse(responseCode = "204", description = "No Content"),
+			@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content) })
+	@DeleteMapping(path = "/{department_id}")
+	public ResponseEntity<Void> deleteById(@PathVariable(name = "department_id") Long id) {
+		service.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@Operation(summary = "Delete all departments")
+	@ApiResponses({ @ApiResponse(responseCode = "204", description = "No Content") })
+	@DeleteMapping
+	public ResponseEntity<Void> deleteAll() {
+		service.deleteAll();
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 }
---
+++ src/main/java/com/project/professor/allocation/service/CourseService.java
@@ -0,0 +1,69 @@
+package com.project.professor.allocation.service;
+
+import org.springframework.stereotype.Service;
+
+import com.project.professor.allocation.entity.Course;
+import com.project.professor.allocation.repository.AllocationRepository;
+import com.project.professor.allocation.repository.CourseRepository;
+import com.project.professor.allocation.repository.DepartmentRepository;
+
+import java.util.List;
+
+@Service
+public class CourseService {
+
+	private CourseRepository repository;
+
+	public CourseService(CourseRepository repository) {
+
+		this.repository = repository;
+
+	}
+
+	public Course findById(Long id) {
+		return repository.findById(id).orElse(null);
+	}
+
+	public List<Course> findAll(String name) {
+
+		if (name == null) {
+			return repository.findAll();
+		} else {
+			return repository.findByNameContainingIgnoreCase(name);
+		}
+
+
+	}
+
+	public Course save(Course course) {
+
+		course.setId(null);
+		return repository.save(course);
+
+
+	}
+
+	public Course update(Course course) {
+		Long id = course.getId();
+		if (id != null && repository.existsById(id)) {
+			return repository.save(course);
+		} else {
+			return null;
+		}
+
+	}
+
+	public void deleteById(Long id) {
+
+
+		if (id != null && repository.existsById(id)) {
+			repository.deleteById(id);
+		}
+	}
+
+	public void deleteAll() {
+
+		repository.deleteAllInBatch();
+
+	}
+}
